85edf49359affeec0bab825872fae167ce257b95
neemapssree <145227095+neemapssree@users.noreply.github.com>
2024-01-17 12:30:57 +0400

    Update loginbox

--- src/Pages/Loginbox.jsx
+++ src/Pages/Loginbox.jsx
@@ -32,6 +32,7 @@ const Loginbox = ({setBoxName}) => {
     e.preventDefault();
     try{      
       if(loginData.email && loginData.password) {
+        console.log(loginData,"login data");
         AxiosInstance.post('/auth/login',loginData).then((res)=>{
           if(res.data.message==="User not found"){
             toast.warning('User not found');            
